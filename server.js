0c98598d5792dd72897fbd8f18ce45116dc20474
Damir Mustafin <frenchthebread@gmail.com>
2016-11-29 00:48:51 +0200

    lint

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 app.set('port', 3000);
 
 app.use('/', express.static(__dirname));
 
-app.listen(app.get('port'), function(){
-  console.log("Server started: http://localhost:"+app.get('port')+"/");
+app.listen(app.get('port'), () => {
+  console.log(`Server started: http://localhost:${app.get('port')}/`);
 });
